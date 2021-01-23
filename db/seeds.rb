709647f187977fa1b0373bde60e1a265ec018037
anil pak <anilpak35@gmail.com>
2021-01-23 15:50:38 +1100

    Rails Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Cocktail.destroy_all
+
+puts "database is clean"
+
+puts 'Creating 5 coctails...'
+
+cocktails_att = [
+  {
+    name: "Light rum",
+    rating: 4,
+    review: Faker::Quote.matz,
+    
+  },
+
+  {
+    name: "Applejack",
+    rating: 3,
+    review: Faker::Quote.matz,
+    
+  },
+
+  {
+    name: "Dark rum",
+    rating: 5,
+    review: Faker::Quote.matz,
+    
+  },
+
+  {
+    name: "Sweet Vermouth",
+    rating: 2,
+    review: Faker::Quote.matz,
+    
+  },
+
+  {
+    name: "Strawberry schnapps",
+    rating:  5,
+    review: Faker::Quote.matz,
+    
+  }
+
+]
+
+Cocktail.create!(cocktails_att)
+puts 'Finished!'
+
+require 'open-uri'
+Ingredient.delete_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = open(url).read
+result = JSON.parse(json)
+result['drinks'].each do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
